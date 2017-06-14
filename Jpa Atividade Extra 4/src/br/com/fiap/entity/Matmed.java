a09223f1980b1b450e2d49a2542739ca44392629
alexandre <alexandrehsantos@gmail.com>
2017-06-13 22:51:44 -0300

    Init

---
+++ Jpa Atividade Extra 4/src/br/com/fiap/entity/Matmed.java
@@ -0,0 +1,46 @@
+package br.com.fiap.entity;
+
+import javax.persistence.Entity;
+
+import org.hibernate.annotations.Table;
+
+@Entity
+public class Matmed {
+	private Integer id;
+	private String descricao; 
+	private Double preco;
+	private String fabricante;
+	private Paciente paciente;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public String getFabricante() {
+		return fabricante;
+	}
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
+	public Paciente getPaciente() {
+		return paciente;
+	}
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+}
---
+++ Jpa Atividade Extra 4/src/br/com/fiap/entity/Procedimento.java
@@ -0,0 +1,34 @@
+package br.com.fiap.entity;
+
+public class Procedimento {
+	private Integer id;
+	private String descricao;
+	private Double preco;
+	private Paciente paciente;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getPreco() {
+		return preco;
+	}
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+	public Paciente getPaciente() {
+		return paciente;
+	}
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+}
